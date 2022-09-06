3d7597ae1e16e2e06b0040ab86e9dbb66da0458c
unknown <afafsyauqi@gmail.com>
2022-09-06 22:10:33 +0700

    fix masalah cors batch 2

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
     'https://www.yoursite.com',
-    'https://spdsoftware.netlify.app/',
+    'https://node-server-skripsi.herokuapp.com',
+    'https://spdsoftware.netlify.app',
     'http://127.0.0.1:5500',
     'http://localhost:3500',
     'http://localhost:3000'

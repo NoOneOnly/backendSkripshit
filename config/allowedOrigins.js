49d3624e801b205a5a6b8df45fbc1cbcb53e1e0c
unknown <afafsyauqi@gmail.com>
2022-09-06 21:57:42 +0700

    fix masalah cors

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,6 @@
 const allowedOrigins = [
     'https://www.yoursite.com',
+    'https://spdsoftware.netlify.app/',
     'http://127.0.0.1:5500',
     'http://localhost:3500',
     'http://localhost:3000'
